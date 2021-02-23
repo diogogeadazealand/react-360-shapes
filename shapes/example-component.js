8290459ab78d4070a6c93928027ecc2326a327a9
Diogo <diog0047@edu.easj.dk>
2021-02-23 09:45:40 +0000

    Added shape template

---
+++ shapes/example-component.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {
+    AppRegistry,
+    StyleSheet,
+    Text,
+    View,
+  } from 'react-360';
+
+class Shape extends React.Component{
+
+    render(){}
+
+}
+AppRegistry.registerComponent('Example', () => Shape);
