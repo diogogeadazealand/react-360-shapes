8e15ef5b7826e329ca0c0857714c129e318d8c3c
Jesper Pedersen <jeppenp12@gmail.com>
2021-03-08 17:03:54 +0100

    Remove obsolete code

--- components/shapes/boxComponent.js
+++ components/shapes/boxComponent.js
@@ -1,9 +1,5 @@
 import React from 'react';
 import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View,
   Box
 } from 'react-360';
 
@@ -15,32 +11,8 @@ export default class BoxComponent extends React.Component {
 
   render() {
     return (
-      <View>
        <Box dimWidth={this.props.width} dimHeight={this.props.height} dimDepth={this.props.length} 
           style={this.props.style}/>
-      </View>
     );
   }
 };
-
-const styles = StyleSheet.create({
-  panel: {
-    // Fill the entire surface
-    width: 1000,
-    height: 600,
-    backgroundColor: 'rgba(255, 255, 255, 0.4)',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  greetingBox: {
-    padding: 20,
-    backgroundColor: '#000000',
-    borderColor: '#639dda',
-    borderWidth: 2,
-  },
-  greeting: {
-    fontSize: 30,
-  },
-});
-
-AppRegistry.registerComponent('Geometry', () => Geometry);
